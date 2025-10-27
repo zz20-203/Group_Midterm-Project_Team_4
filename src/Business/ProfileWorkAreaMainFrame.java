5f0b8f80d323e01e09eea7ba4646d26b0c5f2fec
xu-jer-2025 <xu.jer@northeastern.edu>
2025-10-27 11:31:13 +0900

    Signup Panel Fix

--- src/Business/ProfileWorkAreaMainFrame.java
+++ src/Business/ProfileWorkAreaMainFrame.java
@@ -44,6 +44,7 @@ public class ProfileWorkAreaMainFrame extends javax.swing.JFrame {
      * Creates new form PricingMainFrame
      */
     public static final String HOME_CARD = "studentHome";
+    private static final String SIGNUP_CARD = "signup";
 
     public ProfileWorkAreaMainFrame() {
         initComponents();
@@ -325,8 +326,8 @@ public class ProfileWorkAreaMainFrame extends javax.swing.JFrame {
 
     private void btnSignUpLoginButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSignUpLoginButtonActionPerformed
         SignUpJPanel p = new SignUpJPanel(CardSequencePanel, business, dept);
-        CardSequencePanel.add("signup", p);
-        ((java.awt.CardLayout) CardSequencePanel.getLayout()).next(CardSequencePanel);
+        CardSequencePanel.add(SIGNUP_CARD, p);                     
+        CardLayout cl = (CardLayout) CardSequencePanel.getLayout();    cl.show(CardSequencePanel, SIGNUP_CARD);
     }//GEN-LAST:event_btnSignUpLoginButtonActionPerformed
 
     private void UserNameTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UserNameTextFieldActionPerformed
